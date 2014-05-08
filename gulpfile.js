2ef4471fcf49b21719c697f739ad2435ab639a4c
aaronmars <mars.aaron@gmail.com>
2014-05-08 16:27:16 +0000

    Tweaking the setup

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,26 @@
     var uglify = require('gulp-uglify');
     var jshint = require('gulp-jshint');
     var jscs = require('gulp-jscs');
-
+    function errorReport() {
+        console.log(arguments);
+    }
     gulp.task('inspect', function() {
         return gulp.src('js/**/*.js')
-            .pipe(jshint()).pipe(jshint.reporter('default'))
-            .pipe(jscs());
+            .pipe(jshint())
+            .on('error', function() {})
+            .pipe(jshint.reporter('default'))
+            .pipe(jscs())
+            .on('error', function() {});
     });
     gulp.task('build', function() {
-        return gulp.src('js/**/*.js')
+        return gulp.src([
+                '**/*.js'
+            ])
             .pipe(uglify())
             .pipe(gulp.dest('out'));
+    });
+    gulp.task('watch', ['inspect'], function() {
+        gulp.watch('**/*.js', [ 'inspect' ]);
     });
     gulp.task('default', [ 'inspect', 'build' ]);
 })();
